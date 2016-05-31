d375edc7ccf01b767bf89b041455ac79a0f15d26
Patricia Au <patau@uw.edu>
2016-05-31 09:53:16 -0700

    All pages added and routes working

--- cmd/finalproject/main.go
+++ cmd/finalproject/main.go
@@ -44,9 +44,12 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-		router.GET("/pilot.html", func(c *gin.Context) {
-			c.HTML(http.StatusOK, "pilot.html", nil)
-		})
+	router.GET("/pilot.html", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "pilot.html", nil)
+	})
+	router.GET("/rescuer_edit.html", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "rescuer_edit.html", nil)
+	})
 
 	router.GET("/ping", func(c *gin.Context) {
 		ping := db.Ping()
